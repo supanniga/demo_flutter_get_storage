39115d7264f271c313829bd59bb519dcd46d6188
GEC\supanniga.oga <supanniga.oga@gec.com>
2022-01-10 10:11:24 +0700

    get_storage example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+main() async {
+  print('main');
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Get Storage'),
     );
   }
 }
@@ -29,39 +32,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final box = GetStorage();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  Map<String, bool> defaultSettings = {
+    'sound enable': false,
+    'music enable': false,
+    'show notifications': false,
+  };
+
+  Map<String, dynamic> settings = {};
+
+  @override
+  void initState() {
+    print('initState');
+    print('box : $box');
+    print('box.keys : ${box.getKeys()}');
+    print('box.values : ${box.getValues()}');
+    print('box.toString() : ${box.toString()}');
+
+    settings = box.read('settings') ?? defaultSettings;
+
+    box.listen(() {
+      print('listen');
     });
+
+    box.listenKey('settings', (value) {
+      print('listenKey settings : $value');
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    print('dispose');
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    print('build');
+    print('box : $box');
+    print('box.keys : ${box.getKeys()}');
+    print('box.values : ${box.getValues()}');
+    print('box.toString() : ${box.toString()}');
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          child: Column(
+        children: <Widget>[
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: settings.length,
+              itemBuilder: (BuildContext context, int index) {
+                String key = settings.keys.elementAt(index);
+                return CheckboxListTile(
+                    title: Text(key),
+                    value: settings[key],
+                    onChanged: (value) {
+                      print('onChanged $key $value');
+                      setState(() {
+                        settings[key] = value;
+                      });
+                    });
+              }),
+          ElevatedButton(
+            child: const Text('Save Settings'),
+            onPressed: () async {
+              await box.write('settings', settings);
+              setState(() {
+                print('saved.');
+              });
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Clear Settings'),
+            onPressed: () async {
+              await box.erase();
+              setState(() {
+                print('cleared.');
+              });
+            },
+          ),
+        ],
+      )),
     );
   }
 }
